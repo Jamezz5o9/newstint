b595f2f1262618de1d659bb2f012a432d7851959
Aduloju James <adulojujames@gmail.com>
2022-07-07 11:07:34 +0100

    some new addition

--- src/InitArray.java
+++ src/InitArray.java
@@ -7,8 +7,18 @@ initArray();
 
         System.out.printf("%s%8s%n", "Index", "Value");
 
-        for(int counter =0; counter < array.length; counter++){
+        for(int counter = 0; counter < array.length; counter++){
             System.out.printf("%5d%8d%n", counter, array[counter]);
+        }
+    }
+
+    static void initArray2(){
+        int[] secArray = {0, 0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1};
+
+        System.out.print("Grade distribution");
+
+        for(int grade = 0; grade < secArray.length; grade++){
+
         }
     }
 }
